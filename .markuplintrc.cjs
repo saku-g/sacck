7958fdfdfe188e765377463a343ec900b4912d49
saku-g <sakucode1993@gmail.com>
2024-01-18 00:15:39 +0900

    Change maukuplint config

--- .markuplintrc.cjs
+++ .markuplintrc.cjs
@@ -3,4 +3,12 @@ module.exports = {
   parser: {
     '\\.astro$': '@markuplint/astro-parser',
   },
+  excludeFiles: ['./src/components/Svg.astro'],
+  overrides: {
+    './src/layouts/Layout.astro': {
+      rules: {
+        'character-reference': false,
+      },
+    },
+  },
 };
